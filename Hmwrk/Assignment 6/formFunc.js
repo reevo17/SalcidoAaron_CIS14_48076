debadbfe7aadfd5ad585d842be83180ddcc7268c
reevo17 <runt17@gmail.com>
2014-10-08 15:22:19 -0700

    Final Version Assignement 6 Signed-off-by: reevo17 <runt17@gmail.com>

--- Hmwrk/Assignment 6/formFunc.js
+++ Hmwrk/Assignment 6/formFunc.js
@@ -4,8 +4,8 @@ function getForm(url){
 	var infoSplit=url.split("?");
 	var nameValPairs=infoSplit[1].split("&");
 	var $_GET=new Object;
-		for (var j=0;j<nameValuePairs.length-1;j++){
-			var map=nameValuePairs[j].split("=");
+		for (var j=0;j<nameValPairs.length-1;j++){
+			var map=nameValPairs[j].split("=");
 			var name=map[0];
 			var val = map[1];
 			$_GET[name] = val;
