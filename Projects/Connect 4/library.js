8e00733d7d7dd375fb5437ad91a54c3ad18e48aa
reevo17 <runt17@gmail.com>
2014-10-10 14:30:29 -0700

    Added Variables to Library Signed-off-by: reevo17 <runt17@gmail.com>

--- Projects/Connect 4/library.js
+++ Projects/Connect 4/library.js
@@ -2,3 +2,16 @@
 //	Variable Library 
 //	Array Library
 
+var turn = 0; 		//TURN COUNTER
+var chipA =""; 		//PLAYER 1
+var chipB =""; 		//PLAYER 2
+var playing = true; //Bool to check if playing
+var column = col;
+var row = row;
+var inARow = 0; 	//Counter to see how many chips are in a row
+var occupied = false;
+var h = 0; //Horizontal
+var v = 0; //Verticle
+var d = 0; //Diagnal
+var winner = ""; //Will Show who the winner is in document.write
+var canPlace = true; //Bool to check if the player can place in a row
