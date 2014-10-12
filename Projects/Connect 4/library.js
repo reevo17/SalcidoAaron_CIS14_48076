8c0157613b3fc99e9f77068c4cc8264f7ebb1acb
reevo17 <runt17@gmail.com>
2014-10-12 16:58:44 -0700

    Signed-off-by: reevo17 <runt17@gmail.com>

--- Projects/Connect 4/library.js
+++ Projects/Connect 4/library.js
@@ -6,8 +6,8 @@ var turn = 0; 		 //TURN COUNTER
 var chipA =""; 		 //PLAYER 1
 var chipB =""; 		 //PLAYER 2
 var playing = true;  //Bool to check if playing
-var column = col;
-var row = row;
+var column = col;	 //2D Array
+var row = row;		 //2D Array
 var inARow = 0; 	 //Counter to see how many chips are in a row
 var occupied = false;//Is the space occupied?
 var h = 0; 			 //Horizontal
@@ -15,3 +15,14 @@ var v = 0; 			 //Verticle
 var d = 0; 			 //Diagnal
 var winner = ""; 	 //Will Show who the winner is in document.write
 var canPlace = true; //Bool to check if the player can place in a row
+
+var placeChip = function(){ //Places the chip
+	
+}
+var chipCheck = function(){ //Checks to see if inARow=4
+	
+}
+
+while(playing===true){
+	
+}
