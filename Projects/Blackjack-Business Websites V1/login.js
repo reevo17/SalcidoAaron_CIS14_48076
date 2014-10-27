a338b1c17a383cf86ac0a239eeda555c704d29de
reevo17 <runt17@gmail.com>
2014-10-27 09:47:15 -0700

    Headers updated with newer version and IDs Signed-off-by: reevo17 <runt17@gmail.com>

--- Projects/Blackjack-Business Websites V1/login.js
+++ Projects/Blackjack-Business Websites V1/login.js
@@ -1,8 +1,14 @@
 loggedin = false;
 
 
+
+
 if(loggedin = false)	{
 	document.getElementsByName("loggedin").style.visibility="hidden";
-} else {document.getElementsByName("loggedin").style.visibility="visable";
+	document.getElementsByName("loggedin1").style.visibility="visable";	
+	
+} else {
+	document.getElementsByName("loggedin").style.visibility="visable";
+	document.getElementsByName("loggedin1").style.visibility="hidden";	
 }
 
