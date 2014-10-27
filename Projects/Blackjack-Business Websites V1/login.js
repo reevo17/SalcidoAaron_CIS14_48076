b0c8595988f369155d2874ebab335d3ca7ef9c33
reevo17 <runt17@gmail.com>
2014-10-27 09:37:46 -0700

    Added login.js and an if statement that edits visability of login buttons Signed-off-by: reevo17 <runt17@gmail.com>

---
+++ Projects/Blackjack-Business Websites V1/login.js
@@ -0,0 +1,8 @@
+loggedin = false;
+
+
+if(loggedin = false)	{
+	document.getElementsByName("loggedin").style.visibility="hidden";
+} else {document.getElementsByName("loggedin").style.visibility="visable";
+}
+
