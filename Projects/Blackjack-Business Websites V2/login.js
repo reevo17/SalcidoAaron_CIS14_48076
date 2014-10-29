fa72d2f1c3d9ed85b4dbef5f3aaac962588924e2
reevo17 <runt17@gmail.com>
2014-10-29 16:06:55 -0700

    Eddited register script! Signed-off-by: reevo17 <runt17@gmail.com>

--- Projects/Blackjack-Business Websites V2/login.js
+++ Projects/Blackjack-Business Websites V2/login.js
@@ -1,69 +1,32 @@
 var loggedin = true;
 
-	function validate_Firstname(){
-			var Arr = document.forms["input"]["firstname"].value.match(/^[A-Za-z]+$/g);
+	function validate_Username(){
+			var Arr = document.forms["registerform"]["username"].value.match(/^[A-Za-z]+$/g);
 			if(Arr != null){
 				for (i=0; i<Arr.length;i++){
 					var result ="Arr=["+i+"]="+Arr[i];
 					console.log(result);
 				}
 			}else{
-				alert("Invalid FirstnameField");
+				alert("Invalid Username Field");
 			}
 		}
 		
 		
 
-		function validate_Lastname(){
-			var x = document.Forms["input"]["lastname"].value;
-			var re = new RegExp(/^[A-Za-z]*$/)
-			var m = re.exec(x);
+		function validate_Password(){
+			var Arr = document.forms["registerform"]["password"].value.match(/^[A-Za-z]+$/)
 			if(Arr != null){
 				for (i=0; i<Arr.length;i++){
 					var result ="Arr=["+i+"]="+Arr[i];
 					console.log(result);
 				}
 			}else{
-				alert("Invalid FirstnameField");
+				alert("Invalid Password Field");
 			}
 		}
-				function validate_Birthday(){
-			var x = document.Forms["input"]["birthday"].value;
-			var re = new RegExp(/^[0-9]{2}[-][0-9]{2}[-][0-9]{4}$/g)
-			var m = re.exec(x);
-			if(Arr != null){
-				for (i=0; i<Arr.length;i++){
-					var result ="Arr=["+i+"]="+Arr[i];
-					console.log(result);
-				}
-			}else{
-				alert("Invalid FirstnameField");
-			}
-		}
-				function validate_Email(){
-			var x = document.Forms["input"]["email"].value;
-			var re = new RegExp(/^[A-Za-z]*[@][a-z]+[.][A-Za-z]*$/)
-			var m = re.exec(x);
-			if(Arr != null){
-				for (i=0; i<Arr.length;i++){
-					var result ="Arr=["+i+"]="+Arr[i];
-					console.log(result);
-				}
-			}else{
-				alert("Invalid FirstnameField");
-			}
-		}
-				function validate_Password(){
-			var Arr = document.forms["input"]["password"].value.match(/^[A-Za-z0-9_!$%&]+{5,20}$/g);
-			if(Arr != null){
-				for (i=0; i<Arr.length;i++){
-					var result ="Arr=["+i+"]="+Arr[i];
-					console.log(result);
-				}
-			}else{
-				alert("Invalid FirstnameField");
-			}
-		}
+
+		
 //VALIDATE ENTIRE FORM!
 	function validate_Form(){
 		if(!validate_Firstname()){
