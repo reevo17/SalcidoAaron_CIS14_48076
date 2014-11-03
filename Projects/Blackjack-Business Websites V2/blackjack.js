36d7c3b5153e04c6f20e8dfd9c251fd27ff7cf0b
reevo17 <runt17@gmail.com>
2014-11-03 10:56:18 -0800

    Signed-off-by: reevo17 <runt17@gmail.com>

--- Projects/Blackjack-Business Websites V2/blackjack.js
+++ Projects/Blackjack-Business Websites V2/blackjack.js
@@ -1,9 +1,9 @@
 var playing = true;
 var card = [[],[]];
 
-
-
-
-function drawCard();{
-		
+function loaded() {
+	alert( "THIS IS A TRIAL!<br> Thank you trying out our BlackJack Deluks computer game!<br> This is a game trial, you can play the game a total of 5 times before prompted to purchase the game. <br>" );
 }
+ 
+
+window.onload = loaded();
