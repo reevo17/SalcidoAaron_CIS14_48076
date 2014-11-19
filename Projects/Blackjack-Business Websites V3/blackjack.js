57d87c40df0e968ed06950f2dcde73163340cb6c
reevo17 <runt17@gmail.com>
2014-11-19 11:02:43 -0800

    In Class Edits Signed-off-by: reevo17 <runt17@gmail.com>

--- Projects/Blackjack-Business Websites V3/blackjack.js
+++ Projects/Blackjack-Business Websites V3/blackjack.js
@@ -8,6 +8,21 @@ var moneyDisplay;
 var message;
 var turn = 0;
 
+//CREATE A DECK
+function Deck(suit,number){
+	Math.random();
+}
+
+function loaded() {
+	alert( "THIS IS A TRIAL!<br> Thank you trying out our BlackJack Deluks computer game!<br> This is a game trial, you can play the game a total of 5 times before prompted to purchase the game. <br>" );
+}
+ 
+function dealCard(){
+	for(i in this.cards){
+		this.cards[i] = Math.random();
+		this.cards.Count++;
+	}
+};
 //THE PLAYER
 player = {
 	cards:{
@@ -27,22 +42,6 @@ dealer = {
 	}
 	Hit: dealCard();
 };
-
-
-function newDeck(){
-	Math.random();
-}
-
-function loaded() {
-	alert( "THIS IS A TRIAL!<br> Thank you trying out our BlackJack Deluks computer game!<br> This is a game trial, you can play the game a total of 5 times before prompted to purchase the game. <br>" );
-}
- 
-function dealCard(){
-	for(i in this.cards){
-		this.cards[i] = Math.random();
-		this.cards.Count++;
-	}
-}
 
 window.onload = loaded();
 $( document ).ready(function() {
